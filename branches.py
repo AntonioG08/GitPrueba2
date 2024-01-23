6e1f759e66cc339e88d63e3769d107093ed1de9e
T0ny6969 <togoruiz08@hotmail.com>
2024-01-22 21:48:35 -0600

    Se agrego variable apellido

--- branches.py
+++ branches.py
@@ -4,5 +4,6 @@ x = 3
 print(f'Hola mi nombre es tony y el número es {3}')
 
 nombre = input("Por favor ingresa tu nombre: ")
+apellido = input("Por favor ingrese también su apellido: ")
 
-print(f'Hola {nombre}')
+print(f'Hola {nombre} {apellido}')
